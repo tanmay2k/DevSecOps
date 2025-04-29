356780d97a10345828087d982c8135c96fa4e395
Meet Jamsutkar <91883747+MeJaM35@users.noreply.github.com>
2025-04-30 03:39:41 +0530

    Migration: move to local inference with optimization as well

--- finassist/urls.py
+++ finassist/urls.py
@@ -5,4 +5,5 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('', views.chatbot_view, name="chatbot"),
+    path('test-ollama/', views.test_ollama_connection, name="test_ollama"),
 ]
