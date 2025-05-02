0ced33621abc4dc1ea260dd3bf543c677d05ef98
Tanmay Lad <tanmay.lad.india@gmail.com>
2025-05-02 18:07:07 +0000

    added date

--- finassist/utils.py
+++ finassist/utils.py
@@ -1,10 +1,10 @@
 import json
-from datetime import datetime
+from datetime import datetime, date
 from decimal import Decimal
 
 class DateTimeEncoder(json.JSONEncoder):
     def default(self, obj):
-        if isinstance(obj, datetime):
+        if isinstance(obj, (datetime, date)):
             return obj.isoformat()
         elif isinstance(obj, Decimal):
             return float(obj)
