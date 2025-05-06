e5d2851f5eb3446b5b4a19264b0fda21d851b181
tanmay2k <113576048+tanmay2k@users.noreply.github.com>
2025-05-06 14:23:55 +0530

    Update settings.py
    removed app password

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -155,7 +155,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'tanmay.lad.india@gmail.com'
-EMAIL_HOST_PASSWORD = 'bnthukrwimkrtplz'
+EMAIL_HOST_PASSWORD = '' #write app password here
 DEFAULT_FROM_EMAIL = 'tanmay.lad.india@gmail.com'
 
 
